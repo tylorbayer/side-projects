45eb675272244bc86e215a7f8453ebd85452976c
Tylor Bayer <tylor.bayer@gmail.com>
2021-09-08 23:26:41 -0600

    added review body

--- shakespeare/review-app/src/components/Reviews.js
+++ shakespeare/review-app/src/components/Reviews.js
@@ -51,8 +51,7 @@ export default class Reviews extends React.Component {
                         />
                         <CardContent>
                             <Typography variant="body2" color="textSecondary" component="p">
-                                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                {review.body}
                             </Typography>
                         </CardContent>
                     </Card>
